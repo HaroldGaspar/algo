f2a232a4f1be0c0917f10acfa2004e826509c5b6
Harold <gaspar.hrl@gmail.com>
2020-09-07 20:38:56 -0500

    first commit

---
+++ src/main/java/pe/isil/model/User.java
@@ -0,0 +1,20 @@
+package pe.isil.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+
+    private String name;
+    private String lastNameFather;
+    private String lastNameMother;
+    private Integer age;
+
+
+
+}
+
